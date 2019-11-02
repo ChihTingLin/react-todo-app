8a33e107670ffcf6f5bb31dcbc801157640793b5
Chih-Ting Lin <chihtinglin03@gmail.com>
2019-11-03 00:59:12 +0800

    Add bootstrap css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import TodoList from './components/TodoList'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends React.Component {
   constructor(props) {
